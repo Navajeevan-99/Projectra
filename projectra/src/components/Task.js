b8b6b20ffc045d70b40d8e6dfc24e5456e31814c
Navajeevan-99 <saravanannavajeevan99@gmail.com>
2026-02-16 08:14:10 +0530

    nineth

--- projectra/src/components/Task.js
+++ projectra/src/components/Task.js
@@ -4,11 +4,11 @@ const Task=()=>{
 return (
 
     <div>
-        <div className="taskcard center column">
-        <input type="text" placeholder="Task name"/>
-        <input type="date"/>
-        <input type="date"/>
-        <textarea cols={50} placeholder="Description">
+        <div className="taskcard centervertical column">
+        <input type="text" placeholder="Task name" className='sprintdetailsadd'/>
+        <input type="date" className='sprintdetailsadd'/>
+        <input type="date" className='sprintdetailsadd'/>
+        <textarea cols={50} placeholder="Description" className='sprintdetailsadd' style={{border: 'none',borderBottom: 'solid 2px black',width:'auto',borderRadius:'0px'}}>
         </textarea>
         </div>
     </div>
