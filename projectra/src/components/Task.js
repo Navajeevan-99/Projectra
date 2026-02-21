78e64f50edefae91170b8bb9cd98f3fa4c13d56c
Navajeevan-99 <saravanannavajeevan99@gmail.com>
2026-02-21 16:21:52 +0530

    eleventh

--- projectra/src/components/Task.js
+++ projectra/src/components/Task.js
@@ -3,15 +3,67 @@ import './Task.css'
 const Task=()=>{
 return (
 
-    <div className='center row taskcard'>
+    <div className='center taskcard'>
+        <div className='center row taskcardinner'>
         <div className="centervertical column">
         <input type="text" placeholder="Task name" className='sprintdetailsadd'/>
-        <textarea cols={50} placeholder="Description" className='sprintdetailsadd' style={{border: 'none',borderBottom: 'solid 2px black',width:'auto',borderRadius:'0px'}}>
+        <textarea cols={50} placeholder="Description" className='sprintdetailsadd descriptiontask' style={{width:'auto'}}>
         </textarea>
         </div>
         <div className='centervertical column'>
-        <input type="date" className='sprintdetailsadd'/>
-        <input type="date" className='sprintdetailsadd'/>
+        <table>
+        <tbody>
+            <tr>
+                <td className='taskdetails'>
+                    Assigned for
+                </td>
+                <td></td>
+                <td>
+                    <select>
+                        <option>Jeevan</option>
+                        <option>Leo</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td className='taskdetails'>
+                    Assigned by
+                </td>
+                <td></td>
+                <td>
+                    <p></p>
+                </td>
+            </tr>
+        <tr>
+            <td className='taskdetails'>
+                Start Date
+            </td>
+            <td></td>
+           <td>
+        <input type="date" className='sprintdetailsadd' style={{margin:'0px'}} />
+            </td>
+        </tr>
+        <tr>
+        <td className='taskdetails'>
+            End Date
+        </td>
+        <td></td>
+        <td>
+        <input type="date" className='sprintdetailsadd' style={{margin:'0px'}}/>
+        </td>
+        </tr>
+        <tr>
+        <td className='taskdetails'>
+            Background Color
+        </td>
+        <td></td>
+        <td>
+        <input type='color' className='sprintdetailsadd' style={{width:'100px',margin:'0px'}}/>
+        </td>
+        </tr>
+        </tbody>
+        </table>
+        </div>
         </div>
     </div>
 )
